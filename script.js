028b164e88c8051fc99e245b6809806790b635ed
Maya <mayas2744@tuckahoeschools.org>
2024-11-07 13:29:56 +0000

    second commit

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const greenLight = document.querySelector('.green-light');
 const redSwitch = document.getElementById('red-switch');
 const yellowSwitch = document.getElementById('yellow-switch');
 const greenSwitch = document.getElementById('green-switch');
+const cycleSwitch = document.getElementById('cycle-switch');
+const allSwitch = document.getElementById('all-switch');
 
 //Add event listeners that listen for a button click
 redSwitch.addEventListener('click', () => {
@@ -23,5 +25,38 @@ greenSwitch.addEventListener('click', () => {
 
 
 //Party Mode Button
+let currentLight = 'red';    //initialize current light state
+let intervalId = null;
+
+function cycleLight(){
+    redLight.style.backgroundColor = '#444';
+    yellowLight.style.backgroundColor = '#444';
+    greenLight.style.backgroundColor = '#444';
+    if(currentLight === 'red'){
+        redLight.style.backgroundColor = 'red';
+        currentLight = 'yellow';
+    } else if(currentLight === 'yellow') {
+    yellowLight.style.backgroundColor = 'yellow';
+    currentLight = 'green';
+    } else{
+        greenLight.style.backgroundColor = 'green';
+        currentLight = 'red';
+    }
+}
+
+cycleSwitch.addEventListener('click', () => {
+    if(!intervalId){
+        intervalId = setInterval(cycleLight, 500);
+    }
+    else {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+});
 
 //Turn all lights on and off at once with a single button
+allSwitch.addEventListener('click', () => {
+    redLight.style.backgroundColor = 'red';
+    yellowLight.style.backgroundColor = 'yellow';
+    greenLight.style.backgroundColor = 'green';
+});
